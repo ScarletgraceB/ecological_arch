513b3b5b84fd350a78ed835bdd6c4256fb9a936f
ScarletgraceB <scarlet2.blackall@live.uwe.ac.uk>
2024-02-27 11:00:55 +0000

    adding and testing exit game

---
+++ Assets/Scripts/ExitGame.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitGame : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if esc is pressed - application will quit
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Debug.Log("esc pressed");
+            Application.Quit();
+        }
+    }
+
+
+
+
+}
